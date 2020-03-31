327774d153b889252633b060846ea8b680b2b96a
momobel <plopyomomo@gmail.com>
2020-03-31 23:36:39 +0200

    Use component for graph

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 Vue.component('graph-inst', {
-  props: ['data'],
+  props: ['divid', 'plotData'],
   data: function() {
     return {
-      data: {}
+      divid: null, plotData: null
     }
   },
-  template: '<div id="mygraph"></div>'
+  template: '<div v-bind:id="divid"></div>',
+  mounted: function() {
+    Plotly.newPlot(document.getElementById(this.divid, []))
+  },
+  watch: {
+    plotData: function(val, oldVal) {
+      console.log('new ' + val);
+      if (val == null) {
+        return;
+      }
+      var layout = {yaxis: {type: 'log'}};
+      Plotly.react(document.getElementById(this.divid), val, layout);
+    }
+  }
 })
 
 desired =
@@ -13,12 +26,12 @@ desired =
 
 var app = new Vue({
   el: '#app',
-  data: {countries: desired},
+  data: {countries: desired, graphData: null},
   mounted: function() {
     Plotly.d3.json('https://corona.lmao.ninja/v2/historical', this.parseData);
   },
   methods: {
-    parseData(json) {
+    parseData: function(json) {
       var countries_hist = json.filter(
           entry => entry.province == null && desired.includes(entry.country));
       var data = [];
@@ -28,20 +41,20 @@ var app = new Vue({
         var i = 0;
         var start = false;
         Object.keys(country_data.timeline.cases).forEach(function(day) {
-          var cases = country_data.timeline.cases[day];
-          if (!start && cases > 50) {
+          var ccases = country_data.timeline.cases[day];
+          if (!start && ccases > 50) {
             start = true;
           }
           if (start) {
             x.push(i);
-            y.push(cases);
+            y.push(ccases);
             i = i + 1;
           }
         })
         data.push({name: country_data.country, x: x, y: y})
       }
       var layout = {yaxis: {type: 'log'}};
-      Plotly.newPlot(document.getElementById('graph'), data, layout);
+      this.graphData = data;
     }
   }
 })
